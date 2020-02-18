c9dd39b2959ce8e5b86296a04e9e6c3f414c6a0d
rubensrams <you@example.com>
2020-02-17 23:37:15 -0600

    PRIMER COMMIT BACK

---
+++ app.js
@@ -0,0 +1,31 @@
+//Requeries
+var express = require('express');
+var mongoose = require('mongoose');
+
+//Inicializar variables
+var app = express();
+
+//Conexion a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/HospitalDB', (err, resp) => {
+
+    if (err) throw err;
+
+    console.log('Base de datos : \x1b[32m%s\x1b[0m', 'online');
+
+})
+
+//Rutas
+app.get('/', (req, resp, next) => {
+
+    resp.status(200).json({
+        ok: true,
+        mensaje: 'Peticion correcta'
+    });
+
+});
+
+//Escuchar peticiones
+app.listen(3000, () => {
+
+    console.log('Express server corriendo en el puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+});
